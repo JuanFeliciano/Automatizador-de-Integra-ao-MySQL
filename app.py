88a0d4596f74af79bca7bbf7823b863d9ab79fb8
JuanFeliciano <139025070+JuanFeliciano@users.noreply.github.com>
2023-11-10 16:15:55 -0300

    Projeto Finalizado

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import mysql.connector
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
 from integracao import Ui_MainWindow
 
 conexao = mysql.connector.connect(
@@ -18,11 +18,10 @@ class App(QMainWindow, Ui_MainWindow):
         self.pushButton_7.clicked.connect(self.update)
         self.pushButton_8.clicked.connect(self.delete)
 
-        self.tableWidget_resultados.setColumnCount(2)
-        self.tableWidget_resultados.setHorizontalHeaderLabels(
-            ["Nome do Produto", "Valor"]
+        self.tableWidget.setColumnCount(2)
+        self.tableWidget.setHorizontalHeaderLabels(
+            ["idVendas", "Nome do Produto", "Valor"]
         )
-        self.tableWidget_resultados.addTab(self.tableWidget_resultados, "Resultados")
 
     def create(self):
         nome_produto = self.textEdit.toPlainText()
@@ -36,24 +35,23 @@ class App(QMainWindow, Ui_MainWindow):
         cursor.execute(comando)
         resultados = cursor.fetchall()
 
-        self.tableWidget_resultados.setRowCount(0)
+        self.tableWidget.setRowCount(0)
 
         for row, data in enumerate(resultados):
-            self.tableWidget_resultados.insertRow(row)
+            self.tableWidget.insertRow(row)
             for col, values in enumerate(data):
                 item = QTableWidgetItem(str(values))
-                self.tableWidget_resultados.setItem(row, col, item)
+                self.tableWidget.setItem(row, col, item)
 
     def update(self):
-        nome_produto = self.textEdit_3.toPlainText()
-        valor = self.textEdit_4.toPlainText()
-        idVendas = self.textEdi_6.toPlainText()
-        comando = f'UPDATE vendas SET nome_produto = "{nome_produto}", valor = "{valor}" WHERE idVendas = {idVendas}'
+        nome_produto = self.textEdit_4.toPlainText()
+        idVendas = self.textEdit_6.toPlainText()
+        comando = f'UPDATE vendas SET nome_produto = "{nome_produto}" WHERE idVendas = {idVendas}'
         cursor.execute(comando)
         conexao.commit()
 
     def delete(self):
-        idVendas = self.lineEdit_textEdit_5.text()
+        idVendas = self.textEdit_5.toPlainText()
         comando = f"DELETE FROM vendas WHERE idVendas = {idVendas}"
         cursor.execute(comando)
         conexao.commit()
